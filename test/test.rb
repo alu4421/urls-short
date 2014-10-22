072d042cf6bbc42a2c51041542b2960f834e285c
Karen Mercedes <alu0100402001@ull.edu.es>
2014-10-22 14:36:49 +0100

    añadiendo mas test

--- test/test.rb
+++ test/test.rb
@@ -3,9 +3,9 @@
 
 ENV['RACK_ENV'] = 'test'
 require_relative '../app.rb'
-require 'test/unit'
 require 'minitest/autorun'
 require 'rack/test'
+
 include Rack::Test::Methods
 
 
@@ -18,4 +18,16 @@ describe "Tests de la pagina raiz ('/') con metodo get" do
   get '/'
     assert last_response.ok?
   end
+  
+  #esta pruebas no pasan
+  it "El titulo deberia de ser" do
+    get '/'
+    assert_match "%title SYTW URLS", last_response.body
+  end
+
+  it "El foot deberia de contener" do
+    get '/'
+    assert_match "%p.pull-right Leinah ©Copyright 2014", last_response.body
+  end
+
 end
